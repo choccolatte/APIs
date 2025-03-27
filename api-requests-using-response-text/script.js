ed2b88b0e2e56396ce38aab4a92059b9b1eaf7fb
Aman Singh <124335907+choccolatte@users.noreply.github.com>
2025-03-27 20:46:07 +0530

    Update script.js

--- api-requests-using-response-text/script.js
+++ api-requests-using-response-text/script.js
@@ -4,35 +4,14 @@ const dogURL = 'https://dog.ceo/api/breeds/image/random'
 function getNewDoggos(){
     const promise = fetch(dogURL)
     
-    // promise.then(function(response){
-    //     const processingPromise = response.text()
-    //     return processingPromise
-    // })
-    // .then(function(processedPromise){
-    //     const dogObject = JSON.parse(processedPromise)
-    //     const img = document.createElement('img')
-    //     img.src = dogObject.message
-    //     img.alt = 'Good boys'
-    //     img.style="width:400px;height:400px;"
-    //     dogg.appendChild(img)
-    // })
-    // .catch(function(error){
-    //     //handling the error
-    //     window.alert('🤫Shhhh! Doggos are asleep right now. Try again later!🐶')
-    // })
-
-    // using response.json instead of response.text
-
     promise.then(function(response){
-        const processingPromise = response.json()
+        const processingPromise = response.text()
         return processingPromise
     })
     .then(function(processedPromise){
-        //skipping this, since we already got a parsed json
-        // const dogObject = JSON.parse(processedPromise)
-
+        const dogObject = JSON.parse(processedPromise)
         const img = document.createElement('img')
-        img.src = processedPromise.message
+        img.src = dogObject.message
         img.alt = 'Good boys'
         img.style="width:400px;height:400px;"
         dogg.appendChild(img)
